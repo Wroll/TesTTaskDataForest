eac2102b01150c89f415a8532a7b396995403f91
Wroll <DM.shev4ik@gmail.com>
2020-07-14 14:33:43 +0300

    added new script to scrape data without selenium

--- parse_without_selenium.py
+++ parse_without_selenium.py
@@ -1,23 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
-# import xlsxwriter
+import csv
+import time
 
-url = "https://iwilltravelagain.com"
-# row = 0
-# workbook = xlsxwriter.Workbook('data1.xlsx')
-# worksheet = workbook.add_worksheet()
-#
-#
-# def write_into_excel_document(title, category, location, website, row):
-#     worksheet.write(row, 0, title)
-#     worksheet.write(row, 1, category)
-#     worksheet.write(row, 2, location)
-#     worksheet.write(row, 3, website)
+URL = "https://iwilltravelagain.com"
 
 
-def get_links():
-    links = []
+def write_csv(data):
+    with open('data.csv', 'a', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow((data['title'], data['category'], data['location'], data['link']))
+
+
+def get_response():
     response = requests.get(
         'https://iwilltravelagain.com/wp-json/FH/activities?post_id=143&key=rows_3_grid_activities',
         params={'post_id': '143', 'key': 'rows_3_grid_activities'},
@@ -25,39 +21,39 @@ def get_links():
                  'Expect-CT': 'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"',
                  'cf-request-id': '03eaa4d1b90000f2cc861c1200000001', 'Cache-Control': 'public, max-age=43200',
                  'Link': '<https://iwilltravelagain.com/wp-json/>; rel="https://api.w.org/"'},
-
     )
-    json_response = response.json()
-    for l in json_response:
-        links.append(json_response[l]['link'])
-    h = links[:10]
-    return h
+    return response.json()
+
+
+def get_links():
+    links = []
+    data_from_external_api = get_response()
+    for object in data_from_external_api:
+        links.append(data_from_external_api[object]['link'])
+    return links
 
 
 def parse(partial_link):
-    global row
-    r = requests.get(f"{url + partial_link}")
+    r = requests.get(f"{URL + partial_link}")
     soup = BeautifulSoup(r.text, 'html.parser')
     link = soup.find(alt="Click here to Visit Website", href=True)['href']
     title = soup.find('h1').get_text()
     category = soup.find(class_="quick-details-content").findChildren()[-1].get_text()
     location = soup.find(class_="quick-details").findChildren()[-1].get_text()
-    # write_into_excel_document(title, category, location, link, row)
-    row += 1
+    data = {'title': title, 'category': category, 'location': location, 'link': link}
+    write_csv(data)
     print(f"Title: {title}, Category: {category}, Location: {location}, Link: {link} ")
 
 
-# TODO make a function to write results
 def main():
     links = get_links()
-    with Pool(1) as p:
+    with Pool(60) as p:
         p.map(parse, links)
 
 
 if __name__ == '__main__':
     try:
-        # main()
-        parse(get_links())
+        start = time.time()
+        main()
     finally:
-        pass
-        # workbook.close()
+        print(time.time() - start)
